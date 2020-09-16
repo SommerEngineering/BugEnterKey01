e70bf38cd5b51ad4602c1d294d9d29176343dcf2
Thorsten Sommer <mail@tsommer.org>
2020-09-16 20:41:23 +0200

    Initial

---
+++ BugEnterKey01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace BugEnterKey01
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t1 = new Thread(Reader);
+            t1.IsBackground = false;
+            t1.Start();
+            t1.Join();
+        }
+
+        private static void Reader()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+                    Console.WriteLine($"Key info: char={(int)key.KeyChar}; recognized key={key.Key}");
+                    
+                    if(key.Key == ConsoleKey.Escape)
+                        break;
+                }
+                
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
